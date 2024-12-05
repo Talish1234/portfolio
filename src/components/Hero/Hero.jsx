70971adc4626a323bfd33943cdc35af87fc40baa
Talish1234 <92112059+Talish1234@users.noreply.github.com>
2024-12-05 07:22:03 +0530

    first commit

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import styles from "./Hero.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Hero = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.content}>
+      <h1 className={styles.title}>Hello, I'm Mohd Talish Ansari</h1>
+       <p className={styles.description}>
+        A skilled web developer with expertise in the MERN stack and a focus on full-stack solutions. Feel free to connect for collaboration.
+       </p>
+        <a href="mailto:talishtarik1234@gmail.com" className={styles.contactBtn}>
+          Contact Me
+        </a>
+      </div>
+      <img
+        src={getImageUrl("hero/heroImage.jpg")}
+        alt="Hero image of me"
+        className={styles.heroImg}
+      />
+      <div className={styles.topBlur} />
+      <div className={styles.bottomBlur} />
+    </section>
+  );
+};
